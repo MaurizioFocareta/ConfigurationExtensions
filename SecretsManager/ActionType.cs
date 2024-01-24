8e0d55b303f751d2b2abe3da1eec066ac4de8362
Maurizio Focareta <maurizio.focareta@nais.ai>
2024-01-24 17:17:09 +0100

    Changed parameters format, updated help command.

---
+++ SecretsManager/ActionType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretsManager
+{
+    public enum ActionType
+    {
+        None,
+        ShowHelp,
+        Search,
+        SearchAll,
+        SearchAssembly,
+        Add,
+        Edit,
+        Delete
+    }
+}
--- SecretsManager/Program.cs
+++ SecretsManager/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using System;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Text.RegularExpressions;
 
 namespace SecretsManager
 {
@@ -10,68 +11,144 @@ namespace SecretsManager
     {
         private static IConfigurationRoot configuration;
 
-        private static void HelpString()
+        private static ActionType _actionType = ActionType.None;
+        private static string _searchValue = null;
+        
+        private static bool _impersonate = false;
+        private static string _impersonateUser = null;
+
+        private static void ShowHelp()
         {
             Console.WriteLine("Secrets Manager in Windows CredentialManager (HPE 2024)");
             Console.WriteLine();
-            Console.WriteLine("SecretsManager [-w] [-a] [-p] [-s <Filter>] [-d <Target>]");
+            Console.WriteLine("SecretsManager /u:user [[/w]|[/e:<Target>]|[/a]|[/s:<Filter>]|[/d:<Target>][/p]");
             Console.WriteLine();
-            Console.WriteLine("-w\t\tAdd new generic credentials for the current user");
-            Console.WriteLine("-e <Target>\tEdit generic credentials for the current user with the given <Target>");
-            Console.WriteLine("-a\t\tGet all generic credentials for the current user");
-            Console.WriteLine("-p\t\tGet all generic credentials for the current user with Target based on assembly attribute");
+            Console.WriteLine("/w\t\tAdd new generic credentials for the current user");
+            Console.WriteLine("/e:<Target>\tEdit generic credentials for the current user with the given <Target>");
+            Console.WriteLine("/a\t\tGet all generic credentials for the current user");
+            Console.WriteLine("/s:<Filter>\tGet all generic credentials for the current user with Target starting with <Filter>");
+            Console.WriteLine("/d:<Target>\tRemoves the generic credentials for the current user with the given <Target>");
+            Console.WriteLine("/p\t\tGet all generic credentials for the current user with Target based on assembly attribute");
             Console.WriteLine("\t\t(The assembly is searched for the CredentialManagerPrefixId attribute)");
-            Console.WriteLine("-s <Filter>\tGet all generic credentials for the current user with Target starting with <Filter>");
-            Console.WriteLine("-d <Target>\tRemoves the generic credentials for the current user with the given <Target>");
             Console.WriteLine();
         }
 
         static void Main(string[] args)
         {
-            if (args.Length == 1)
+            if (args.Length == 0)
             {
-                if (args[0] == "-w")
-                {
-                    AddCredential();
-                }
-                else if (args[0] == "-a")
-                {
-                    GetConfiguration(string.Empty);
-                }
-                else if (args[0] == "-p")
-                {
-                    GetConfiguration<Program>();
-                }
-                else
-                {
-                    Console.WriteLine($"Unknown parameter {args[0]}");
-                    HelpString();
-                }
-            }
-            else if (args.Length == 2)
-            {
-                if (args[0] == "-s")
-                {
-                    GetConfiguration(args[1]);
-                }
-                else if (args[0] == "-d")
-                {
-                    RemoveCredential(args[1]);
-                }
-                else if (args[0] == "-e")
-                {
-                    EditCredential(args[1]);
-                }
-                else
-                {
-                    Console.WriteLine($"Unknown parameter {args[0]}");
-                    HelpString();
-                }
+                ShowHelp();
             }
             else
             {
-                HelpString();
+                if (ParseParameters(args))
+                {
+                    switch (_actionType)
+                    {
+                        case ActionType.ShowHelp:
+                            ShowHelp();
+                            break;
+                        case ActionType.Search:
+                            GetConfiguration(_searchValue);
+                            break;
+                        case ActionType.SearchAll:
+                            GetConfiguration(string.Empty);
+                            break;
+                        case ActionType.SearchAssembly:
+                            GetConfiguration<Program>();
+                            break;
+                        case ActionType.Add:
+                            AddCredential();
+                            break;
+                        case ActionType.Edit:
+                            EditCredential(_searchValue);
+                            break;
+                        case ActionType.Delete:
+                            RemoveCredential(_searchValue);
+                            break;
+                        default:
+                            Console.WriteLine($"Select an action. Use /h or no argument for the help.");
+                            break;
+                    }
+                }
             }
+        }
+
+        static bool ParseParameters(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                Regex argRegEx = new Regex("/(?<Switch>\\w):?(?<Value>.*)?");
+                var match = argRegEx.Match(arg);
+                if (!match.Success)
+                {
+                    Console.WriteLine($"Invalid option - \"{arg}\"");
+                    return false;
+                }
+
+                if (_actionType != ActionType.None)
+                {
+                    Console.WriteLine($"Use only one option between /w /e /a /s /d /p");
+                    return false;
+                }
+
+                var action = match.Groups["Switch"].Value;
+                var value = match.Groups["Value"].Value;
+
+                switch (action)
+                {
+                    case "w":
+                        _actionType = ActionType.Add;
+                        break;
+                    case "e":
+                        _actionType = ActionType.Edit;
+                        _searchValue = value;
+                        if (string.IsNullOrEmpty(_searchValue))
+                        {
+                            Console.WriteLine($"Missing value in \"/e\" option");
+                            return false;
+                        }
+                        break;
+                    case "a":
+                        _actionType = ActionType.SearchAll;
+                        break;
+                    case "s":
+                        _actionType = ActionType.Search;
+                        _searchValue = value;
+                        if (string.IsNullOrEmpty(_searchValue))
+                        {
+                            Console.WriteLine($"Missing value in \"/s\" option");
+                            return false;
+                        }
+                        break;
+                    case "d":
+                        _actionType = ActionType.Delete;
+                        _searchValue = value;
+                        if (string.IsNullOrEmpty(_searchValue))
+                        {
+                            Console.WriteLine($"Missing value in \"/d\" option");
+                            return false;
+                        }
+                        break;
+                    case "p":
+                        _actionType = ActionType.SearchAssembly;
+                        break;
+                    case "u":
+                        _impersonate = true;
+                        _impersonateUser = value;
+                        if (string.IsNullOrEmpty(_impersonateUser))
+                        {
+                            Console.WriteLine($"Missing value in \"/u\" option");
+                            return false;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine($"Unknown option - \"/{action}\"");
+                        return false;
+                }
+            }
+
+            return true;
         }
 
         private static void EditCredential(string targetName)
